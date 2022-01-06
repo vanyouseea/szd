7a1ecce5d5e18e8f6fef1ea64c3db0896f0e24ea
He Rui Long  (NCS) <ruilong@ncs.com.sg>
2022-01-06 17:19:43 +0800

    ss

--- src/main/java/hqr/szd/ctrl/IndexCtrl.java
+++ src/main/java/hqr/szd/ctrl/IndexCtrl.java
@@ -3,12 +3,18 @@ package hqr.szd.ctrl;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import hqr.szd.dao.TaUserRepo;
+import hqr.szd.domain.TaUser;
 import hqr.szd.service.CallBackService;
 import hqr.szd.service.GetGlobalInd;
+import hqr.szd.service.GetUserSubDomainInfo;
 
 @Controller
 public class IndexCtrl {
@@ -18,6 +24,12 @@ public class IndexCtrl {
 	
 	@Autowired
 	private CallBackService cbs;
+	
+	@Autowired
+	private TaUserRepo tur;
+	
+	@Autowired
+	GetUserSubDomainInfo gusd;
 	
 	@RequestMapping(value = {"/","/index.html"})
 	public String index(HttpServletRequest request) {
@@ -31,6 +43,15 @@ public class IndexCtrl {
 			System.out.println("Not allow to reg admin");
 			return "login";
 		}
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = {"/getSubDomainInfo"})
+	public String getSubDomainInfo(){
+		UserDetails ud = (UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		TaUser enti = tur.getUserById(ud.getUsername());
+		int mySeqNo = enti.getSeqNo();
+		return gusd.getSubDomainCntInfo(mySeqNo);
 	}
 	
 	@ResponseBody
